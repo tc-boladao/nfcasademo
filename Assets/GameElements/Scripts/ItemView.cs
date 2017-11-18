99590a6be0d6d471d71d0305b6cc602fba2e3ca2
feldavol <feldavol@hotmail.com>
2017-11-18 04:33:18 -0200

    Bug Da Chave

--- Assets/GameElements/Scripts/ItemView.cs
+++ Assets/GameElements/Scripts/ItemView.cs
@@ -3,27 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ItemView : MonoBehaviour {
-
+    private int index = 0;
+    private GameObject[] itemList;
 
     // Use this for initialization
     void Start () {
-        ViewKey();
+        index = PlayerPrefs.GetInt("ItemList", index);
+        itemList = new GameObject[transform.childCount];
+        transform.GetChild(0).gameObject.SetActive(false);
+
     }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        ViewKey();
+    }
 
     public void ViewKey() {
-        if (GameManager.instance.haveKey.Equals(false))
-        {
-            gameObject.SetActive(false);
-        }
-        else
-        {
-            gameObject.SetActive(true);
-        }
+            itemList[0] = transform.GetChild(0).gameObject;
+            if (GameManager.instance.haveKey.Equals(true))
+            {
+                itemList[0].SetActive(true);
+            }
+        
 
     }
 }
